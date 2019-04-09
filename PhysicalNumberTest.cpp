2abc4770633f8b125ca5e74fd3053f5f9b308f42
YuvalAmar <yuvalamar75@gmail.com>
2019-04-09 13:10:16 +0300

    Add new tests

--- PhysicalNumberTest.cpp
+++ PhysicalNumberTest.cpp
@@ -21,36 +21,66 @@ int main() {
     // BASIC TESTS - DO NOT CHANGE
     PhysicalNumber a(2, Unit::KM);
     PhysicalNumber b(300, Unit::M);
+	PhysicalNumber i(100, Unit::CM);
+
     PhysicalNumber c(2, Unit::HOUR);
     PhysicalNumber d(30, Unit::MIN);
+	PhysicalNumber e(10,Unit::SEC);
+	PhysicalNumber f(3,Unit::TON);
+	PhysicalNumber g(10,Unit::KG);
+	PhysicalNumber h(2,Unit::G);
 
     testcase
     .setname("Basic output")
-    .CHECK_OUTPUT(a, "2[km]")
-    .CHECK_OUTPUT(b, "300[m]")
+    .CHECK_OUTPUT(c, "2[hour]")
+    .CHECK_OUTPUT(e, "10[sec]")
+	.CHECK_OUTPUT(f,"3[ton]")
+	.CHECK_OUTPUT(d,"30[min]")
+	.CHECK_OUTPUT(i,"100[cm]")
+
 
     .setname("Compatible dimensions")
+	
     .CHECK_OUTPUT(b+a, "2300[m]")
     .CHECK_OUTPUT((a+=b), "2.3[km]")
+	.CHECK_OUTPUT((f+g), "3.01[ton]")
+	.CHECK_OUTPUT((d++), "31[min]")
     .CHECK_OUTPUT(a, "2.3[km]")
     .CHECK_OUTPUT(a+a, "4.6[km]")
     .CHECK_OUTPUT(b-b, "0[m]")
+	.CHECK_OUTPUT((a+=b), "2.6[km]")
     .CHECK_OUTPUT(c, "2[hour]")
-    .CHECK_OUTPUT(d, "30[min]")
-    .CHECK_OUTPUT(d+c, "150[min]")
+    .CHECK_OUTPUT(d, "31[min]")
+    .CHECK_OUTPUT(d+c, "151[min]")
+	.CHECK_OUTPUT((f+g), "3.01[ton]")
+	.CHECK_OUTPUT((g+f), "3010[kg]")
+	.CHECK_OUTPUT(c--, "1[hour]")
+	
+
+
 
     .setname("Incompatible dimensions")
     .CHECK_THROWS(a+c)
     .CHECK_THROWS(a+d)
     .CHECK_THROWS(b+c)
-    .CHECK_THROWS(b+d)
+    .CHECK_THROWS(e+f)
+	.CHECK_THROWS(f+a)
+	.CHECK_THROWS(b+d)
+	.CHECK_THROWS(h+e)
+	
 
     .setname("Basic input")
     .CHECK_OK(istringstream("700[kg]") >> a)
+	.CHECK_OK(istringstream("100[sec]") >> b)
+	.CHECK_OK(istringstream("300[g]") >> c)
+	.CHECK_OK(istringstream("100[m]") >> d)
     .CHECK_OUTPUT((a += PhysicalNumber(1, Unit::TON)), "1700[kg]")
+	.CHECK_OUTPUT((d += PhysicalNumber(10, Unit::KM)), "10100[m]")
+	.CHECK_OUTPUT((a -= PhysicalNumber(300, Unit::KG)), "1400[kg]")
 
-    // YOUR TESTS - INSERT AS MANY AS YOU WANT
 
+
+    
       .setname("...")
 
       .print(cout, /*show_grade=*/false);
