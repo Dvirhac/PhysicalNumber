2698c02a769a0127d34eb4daaf24d83642ce3cf6
yuvalamar <44939281+yuvalamar75@users.noreply.github.com>
2019-04-14 11:06:55 +0300

    Update PhysicalNumber.h

--- PhysicalNumber.h
+++ PhysicalNumber.h
@@ -47,7 +47,7 @@ namespace ariel{
 
         //operators
         PhysicalNumber operator+ (const PhysicalNumber& f);
-        const PhysicalNumber operator- (const PhysicalNumber& F2);
+        PhysicalNumber operator- (const PhysicalNumber& F2);
         PhysicalNumber operator- ();
         PhysicalNumber& operator+ ();
         PhysicalNumber& operator+=(const PhysicalNumber& F);
